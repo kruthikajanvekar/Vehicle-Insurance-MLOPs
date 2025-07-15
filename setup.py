57cb98dc78f9344cb4a37ef3a77b4f2f48c5d80e
kruthikajanvekar <kruthika7824@gmail.com>
2025-07-15 14:57:49 +0530

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Kruthika Janvekar",
+    author_email="kruthika7824@gmail.com",
+    packages=find_packages()
+)
